87477e17ff374b0f15dd00c04acf4bf9243d0896
jeanciok <jeanchristianm@gmail.com>
2023-11-27 16:49:20 -0300

    add StateConfigurations

---
+++ CustomerManagament.Infrastructure/Persistence/Configuration/StateConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using CustomerManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagament.Infrastructure.Persistence.Configuration
+{
+    public class StateConfigurations : IEntityTypeConfiguration<State>
+    {
+        public void Configure(EntityTypeBuilder<State> builder)
+        {
+            builder
+                .HasKey(p => p.Id);
+        }
+    }
+}
