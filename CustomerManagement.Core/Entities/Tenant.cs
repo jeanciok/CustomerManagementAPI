ed71fca0b09737995a0d55f07fa00f320906c7f1
jeanciok <jeanchristianm@gmail.com>
2023-11-27 16:33:59 -0300

    updated Tenant Entity

--- CustomerManagement.Core/Entities/Tenant.cs
+++ CustomerManagement.Core/Entities/Tenant.cs
@@ -8,17 +8,23 @@ namespace CustomerManagement.Core.Entities
 {
     public class Tenant
     {
-        public Tenant(string name, string slug)
+        public Tenant(string name, bool isActive, string slug, ICollection<User> users, ICollection<Customer> customers, ICollection<CustomerGroup> customerGroups)
         {
             Name = name;
+            IsActive = isActive;
             Slug = slug;
-            IsActive = true;
+            Users = users;
+            Customers = customers;
+            CustomerGroups = customerGroups;
         }
-
 
         public Guid TenantId { get; set; }
         public string Name { get; set; }
         public bool IsActive { get; set; }
         public string Slug { get; set; }
+
+        public ICollection<User> Users { get; set; }
+        public ICollection<Customer> Customers { get; set; }
+        public ICollection<CustomerGroup> CustomerGroups { get; set; }
     }
 }
