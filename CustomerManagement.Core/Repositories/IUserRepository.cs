25d16b7a6585bad4407a47e67e244718a9d233c5
jean.andrade <jean.andrade@actdigital.com>
2023-11-28 10:25:58 -0300

    add IUserRepository

---
+++ CustomerManagement.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using CustomerManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllUserAsync();
+        Task<User> GetUserByIdAsync(Guid id);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(Guid id);
+    }
+}
