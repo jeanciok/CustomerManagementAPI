a8b8e107730b8beb37d5eb788397616533536143
jeanciok <jeanchristianm@gmail.com>
2023-12-02 11:34:15 -0300

    addes StateRepository

---
+++ CustomerManagament.Infrastructure/Persistence/Repositories/StateRepository.cs
@@ -0,0 +1,29 @@
+﻿using CustomerManagement.Core.Entities;
+using CustomerManagement.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagament.Infrastructure.Persistence.Repositories
+{
+    public class StateRepository : IStateRepository
+    {
+        private readonly CustomerManagementDbContext _dbContext;
+        public StateRepository(CustomerManagementDbContext dbContext)
+
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<State>> GetAll()
+        {
+            List<State> states = await _dbContext
+                .State.ToListAsync();
+
+            return states;
+        }
+    }
+}
---
+++ CustomerManagement.Core/Repositories/IStateRepository.cs
@@ -0,0 +1,14 @@
+﻿using CustomerManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Core.Repositories
+{
+    public interface IStateRepository
+    {
+        Task<List<State>> GetAll();
+    }
+}
