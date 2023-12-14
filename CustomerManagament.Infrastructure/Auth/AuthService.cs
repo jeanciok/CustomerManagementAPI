adbcebf655537c7847fc59e5c7f65c437bbe6482
jeanciok <jeanchristianm@gmail.com>
2023-12-14 17:54:09 -0300

    feat: add AuthService

---
+++ CustomerManagament.Infrastructure/Auth/AuthService.cs
@@ -0,0 +1,62 @@
+﻿using CustomerManagement.Core.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+
+namespace CustomerManagament.Infrastructure.Auth
+{
+    internal class AuthService : IAuthService
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string ComputeSha256Hash(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new();
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        public string GenerateToken(string email, string role)
+        {
+            var issuer = _configuration["Jwt:Issuer"];
+            var audience = _configuration["Jwt:Audience"];
+            var key = _configuration["Jwt:Key"];
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            List<Claim> claims = new()
+            {
+                new Claim("username", email),
+                new Claim("ClaimTypes.Role", role),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials,
+                claims: claims);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ CustomerManagement.Core/Auth/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Core.Services
+{
+    public interface IAuthService
+    {
+        string ComputeSha256Hash(string password);
+        string GenerateToken(string username, string password);
+    }
+}
