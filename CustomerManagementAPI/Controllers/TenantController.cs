ec82b8ec94b8abaa5e3961a9f71c2132ca2f66ce
jeanciok <jeanchristianm@gmail.com>
2024-01-29 10:06:16 -0300

    feat: add TenantController

---
+++ CustomerManagementAPI/Controllers/TenantController.cs
@@ -0,0 +1,26 @@
+﻿using CustomerManagement.Application.Commands.CreateTenant;
+using CustomerManagement.Application.ViewModels;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class TenantController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TenantController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateTenantCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+    }
+}
