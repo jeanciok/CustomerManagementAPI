1dd5a2d47fbe7af8015dfd44a6447fbd0b762218
jeanciok <jeanchristianm@gmail.com>
2024-01-29 09:49:05 -0300

    feat: add StringExtensions

---
+++ CustomerManagement.Core/Helpers/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Core.Helpers
+{
+    public static class StringExtensions
+    {
+        public static string ToSlug(this string text)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(
+                System.Text.RegularExpressions.Regex.Replace(
+                    System.Text.RegularExpressions.Regex.Replace(
+                        text.ToLower(),
+                        @"\s", "-",
+                        System.Text.RegularExpressions.RegexOptions.Compiled),
+                    @"[^a-z0-9\s-_]", "",
+                    System.Text.RegularExpressions.RegexOptions.Compiled),
+                @"-+", "-",
+                System.Text.RegularExpressions.RegexOptions.Compiled).Trim('-');
+        }
+    }
+}
