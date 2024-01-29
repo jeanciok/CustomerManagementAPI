65040fff3c6cd08e69422fd50ffd4e7d4bb87472
jeanciok <jeanchristianm@gmail.com>
2024-01-29 09:52:47 -0300

    feat: add TenantRepository

---
+++ CustomerManagament.Infrastructure/Persistence/Repositories/TenantRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerManagament.Infrastructure.Persistence;
+using CustomerManagement.Core.Entities;
+using CustomerManagement.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerManagement.Infrastructure.Repositories
+{
+    public class TenantRepository : ITenantRepository
+    {
+        private readonly CustomerManagementDbContext _context;
+
+        public TenantRepository(CustomerManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Tenant> GetByIdAsync(Guid id)
+        {
+            return await _context.Tenants
+                .FirstOrDefaultAsync(c => c.TenantId == id);
+        }
+
+        public async Task<List<Tenant>> GetAllAsync()
+        {
+            return await _context.Tenants
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Tenant tenant)
+        {
+            await _context.AddAsync(tenant);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Tenant tenant)
+        {
+            _context.Tenants.Update(tenant);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ CustomerManagement.Core/Repositories/ITenantRepository.cs
@@ -0,0 +1,12 @@
+﻿using CustomerManagement.Core.Entities;
+
+namespace CustomerManagement.Core.Interfaces
+{
+    public interface ITenantRepository
+    {
+        Task<Tenant> GetByIdAsync(Guid id);
+        Task<List<Tenant>> GetAllAsync();
+        Task AddAsync(Tenant tenant);
+        Task UpdateAsync(Tenant tenant);
+    }
+}
