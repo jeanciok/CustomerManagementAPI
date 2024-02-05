46613863121d4ea2cab9eb66f58c4e47da74c4be
jeanciok <jeanchristianm@gmail.com>
2024-02-05 11:04:07 -0300

    test: add CreateCustomerGroupCommandValidatorTest

---
+++ CustomerManagement.Application/Validators/CreateCustomerGroupCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using CustomerManagement.Application.Commands.CreateCustomerGroup;
+using FluentValidation;
+
+namespace CustomerManagement.Application.Validators
+{
+    public class CreateCustomerGroupCommandValidator : AbstractValidator<CreateCustomerGroupCommand>
+    {
+        public CreateCustomerGroupCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        }
+    }
+}
---
+++ CustomerManagement.UnitTests/Application/Commands/Validators/CreateCustomerGroupCommandValidatorTest.cs
@@ -0,0 +1,45 @@
+﻿using CustomerManagement.Application.Commands.CreateCustomerGroup;
+using CustomerManagement.Application.Validators;
+using Xunit;
+
+namespace CustomerManagement.UnitTests.Application.Commands.Validators
+{
+    public class CreateCustomerGroupCommandValidatorTest
+    {
+        [Fact]
+        public void InputDataIsOk_Validate_ShouldNotHaveValidationError()
+        {
+            // Arrange
+            var validator = new CreateCustomerGroupCommandValidator();
+            var command = new CreateCustomerGroupCommand
+            {
+                Name = "Group 1"
+            };
+
+            // Act
+            var result = validator.Validate(command);
+
+            // Assert
+            Assert.True(result.IsValid);
+            Assert.Empty(result.Errors);
+        }
+
+        [Fact]
+        public void EmptyName_Validate_ShouldHaveValidationError()
+        {
+            // Arrange
+            var validator = new CreateCustomerGroupCommandValidator();
+            var command = new CreateCustomerGroupCommand
+            {
+                Name = ""
+            };
+
+            // Act
+            var result = validator.Validate(command);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, error => error.ErrorMessage == "Name is required");
+        }
+    }
+}
