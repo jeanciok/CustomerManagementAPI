42e148d5ae2befd085b7dba2f45dbbbb25761f1a
jeanciok <jeanchristianm@gmail.com>
2024-03-07 11:08:20 -0300

    chore: migrations

--- CustomerManagament.Infrastructure/Migrations/20240307140724_Update_Customer_PascalCase.Designer.cs
+++ CustomerManagament.Infrastructure/Migrations/20240307140724_Update_Customer_PascalCase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CustomerManagament.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CustomerManagament.Infrastructure.Migrations
 {
     [DbContext(typeof(CustomerManagementDbContext))]
-    partial class CostumerManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240307140724_Update_Customer_PascalCase")]
+    partial class Update_Customer_PascalCase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CustomerManagament.Infrastructure/Migrations/20240307140724_Update_Customer_PascalCase.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerManagament.Infrastructure.Migrations
+{
+    public partial class Update_Customer_PascalCase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RG",
+                table: "Customers",
+                newName: "Rg");
+
+            migrationBuilder.RenameColumn(
+                name: "CPF",
+                table: "Customers",
+                newName: "Cpf");
+
+            migrationBuilder.RenameColumn(
+                name: "CNPJ",
+                table: "Customers",
+                newName: "Cnpj");
+
+            migrationBuilder.RenameColumn(
+                name: "CEP",
+                table: "Customers",
+                newName: "Cep");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Rg",
+                table: "Customers",
+                newName: "RG");
+
+            migrationBuilder.RenameColumn(
+                name: "Cpf",
+                table: "Customers",
+                newName: "CPF");
+
+            migrationBuilder.RenameColumn(
+                name: "Cnpj",
+                table: "Customers",
+                newName: "CNPJ");
+
+            migrationBuilder.RenameColumn(
+                name: "Cep",
+                table: "Customers",
+                newName: "CEP");
+        }
+    }
+}
