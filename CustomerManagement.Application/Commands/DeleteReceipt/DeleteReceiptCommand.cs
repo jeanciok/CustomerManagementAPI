bc3954982e6dbf6e2d0a7c9f09590fb7ca130438
jeanciok <jeanchristianm@gmail.com>
2024-06-18 14:05:10 -0300

    feat: DeleteReceiptCommand

---
+++ CustomerManagement.Application/Commands/DeleteReceipt/DeleteReceiptCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+
+namespace CustomerManagement.Application.Commands.DeleteReceipt
+{
+    public class DeleteReceiptCommand : IRequest<Unit>
+    {
+        public Guid ReceiptId { get; }
+
+        public DeleteReceiptCommand(Guid receiptId)
+        {
+            ReceiptId = receiptId;
+        }
+    }
+}
---
+++ CustomerManagement.Application/Commands/DeleteReceipt/DeleteReceiptCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using CustomerManagement.Application.Commands.DeleteReceipt;
+using CustomerManagement.Core.Repositories;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Application.Commands.DeleteReceipt
+{
+    public class DeleteReceiptCommandHandler : IRequestHandler<DeleteReceiptCommand, Unit>
+    {
+        private readonly IReceiptRepository _receiptRepository;
+
+        public DeleteReceiptCommandHandler(IReceiptRepository receiptRepository)
+        {
+            _receiptRepository = receiptRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteReceiptCommand request, CancellationToken cancellationToken)
+        {
+            await _receiptRepository.DeleteAsync(request.ReceiptId);
+            return Unit.Value;
+        }
+    }
+}
